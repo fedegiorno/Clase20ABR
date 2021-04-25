843234ae841195151bfdd6c0ae1455fabaf3b219
fedegiorno <fedegiorno@gmail.com>
2021-04-25 18:43:25 -0300

    Terminado con errores

--- app/src/main/java/com/fedegiorno/clase20abr/fragmentos/FragmentoLogin.kt
+++ app/src/main/java/com/fedegiorno/clase20abr/fragmentos/FragmentoLogin.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import androidx.navigation.findNavController
+
 import android.view.ViewGroup
+import android.widget.Button
 import com.fedegiorno.clase20abr.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -13,9 +21,9 @@ import com.fedegiorno.clase20abr.R
  * create an instance of this fragment.
  */
 class FragmentoLogin : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var v: View
+    lateinit var btnAcceder: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,31 +31,22 @@ class FragmentoLogin : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        v=inflater.inflate(R.layout.fragment_fragmento_login, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento_login, container, false)
+        btnAcceder = v.findViewById(R.id.btnAcceder)
+
+        return v}
+
+    override fun onStart() {
+        super.onStart()
+        btnAcceder.setOnClickListener{
+
+            val action2 = FragmentoLoginDirections.actionFragmentoLoginToMainActivityListaCursos2()
+            v.findNavController().navigate(action2)
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentoLogin.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentoLogin().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
